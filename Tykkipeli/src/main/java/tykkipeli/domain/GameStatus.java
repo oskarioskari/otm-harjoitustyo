6199e94c55f5c31e70ab5a4ee9c1854d6a2fed02
Oskari <oskari.tommiska@helsinki.fi>
2018-04-09 10:50:07 +0300

    Add ability to track scores

--- Tykkipeli/src/main/java/tykkipeli/domain/GameStatus.java
+++ Tykkipeli/src/main/java/tykkipeli/domain/GameStatus.java
@@ -1,29 +1,39 @@
-
 package tykkipeli.domain;
 
 public class GameStatus {
+
     private int playerInTurn;
     private int wait;
-    
+    private int[] playerScores;
+
     public GameStatus() {
         this.playerInTurn = 0;
         this.wait = 0;
+        this.playerScores = new int[]{0, 0};
     }
-    
+
     public void setTurn(int player) {
         this.playerInTurn = player;
     }
-    
+
     public void setWait(int wait) {
         this.wait = wait;
     }
-    
+
     public int getTurn() {
         return this.playerInTurn;
     }
-    
+
     public int getWait() {
         return this.wait;
     }
-    
+
+    public void addPoint(int player) {
+        this.playerScores[player]++;
+    }
+
+    public int[] getPlayerScores() {
+        return this.playerScores;
+    }
+
 }
