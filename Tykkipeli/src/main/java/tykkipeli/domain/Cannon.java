daf2a71cb0362ec7ff6b4eab08cd156bb00daa1e
Oskari <oskari.tommiska@helsinki.fi>
2018-04-09 19:24:30 +0300

    Add ability to aim cannon properly

--- Tykkipeli/src/main/java/tykkipeli/domain/Cannon.java
+++ Tykkipeli/src/main/java/tykkipeli/domain/Cannon.java
@@ -1,6 +1,7 @@
 package tykkipeli.domain;
 
 public class Cannon extends GraphicObject {
+
     private double angle;
     private double power;
 
@@ -9,27 +10,35 @@ public class Cannon extends GraphicObject {
         this.angle = -1.0 * Math.PI / 2;
         this.power = 2;
     }
-    
+
     public Cannon(double x, double y, double cannonAngle, double shootingPower) {
         super(x, y);
         this.angle = cannonAngle;
         this.power = shootingPower;
     }
-    
+
     public double getCannonAngle() {
         return this.angle;
     }
-    
-    public double getCannonPower() {
-        return this.power;
-    }
-    
+
     public void setCannonAngle(double angle) {
         this.angle = angle;
     }
-    
+
+    public void increaseCannonAngle(double angle) {
+        this.angle += angle;
+    }
+
+    public double getCannonPower() {
+        return this.power;
+    }
+
     public void setCannonPower(double power) {
         this.power = power;
+    }
+
+    public void increaseCannonPower(double power) {
+        this.power += power;
     }
 
 }
