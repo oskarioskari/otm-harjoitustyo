a6d5480d18a6924ace0223aab13f37c18845e8b5
Oskari <oskari.tommiska@helsinki.fi>
2018-04-17 18:12:14 +0300

    Update GameStatus.java

--- Tykkipeli/src/main/java/tykkipeli/domain/GameStatus.java
+++ Tykkipeli/src/main/java/tykkipeli/domain/GameStatus.java
@@ -1,25 +1,34 @@
 package tykkipeli.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameStatus {
 
     private int playerInTurn;
     private int wait;
+    private int[] waitOver;
     private int[] playerScores;
-    private int selectedWeapon;
+    private int[] selectedWeapon;
     private double[] gravity;
     private final List<Player> playerList;
-    private final List<GraphicObject> ammoList;
+    private final List<GraphicObject> ammoListPlayer1;
+    private final List<GraphicObject> ammoListPlayer2;
+    private final List<List<GraphicObject>> ammoLists;
 
-    public GameStatus(List<Player> playerList, List<GraphicObject> ammolist, double[] worldGravity) {
+    public GameStatus(List<Player> playerList, List<GraphicObject> ammolistPlayer1, List<GraphicObject> ammolistPlayer2, double[] worldGravity) {
         this.playerInTurn = 0;
         this.wait = 0;
+        this.waitOver = new int[]{0, 0};
         this.playerScores = new int[]{0, 0};
-        this.selectedWeapon = 0;
+        this.selectedWeapon = new int[]{0, 0};
         this.gravity = worldGravity;
         this.playerList = playerList;
-        this.ammoList = ammolist;
+        this.ammoListPlayer1 = ammolistPlayer1;
+        this.ammoListPlayer2 = ammolistPlayer2;
+        this.ammoLists = new ArrayList<>();
+        this.ammoLists.add(ammoListPlayer1);
+        this.ammoLists.add(ammoListPlayer2);
     }
 
     public void setTurn(int player) {
@@ -38,6 +47,14 @@ public class GameStatus {
         return this.wait;
     }
 
+    public void setWaitOver(int player, int value) {
+        this.waitOver[player] = value;
+    }
+
+    public int getWaitOver(int player) {
+        return this.waitOver[player];
+    }
+
     public void addPoint(int player) {
         if (player == 0 || player == 1) {
             this.playerScores[player]++;
@@ -52,20 +69,20 @@ public class GameStatus {
         if (player == 0 || player == 1) {
             return this.playerScores[player];
         } else {
-            return -1;
+            return 0;
         }
     }
 
-    public void setSelectedWeapon(int w) {
-        this.selectedWeapon = w;
+    public void setSelectedWeapon(int player, int weapon) {
+        this.selectedWeapon[player] = weapon;
     }
 
-    public int getSelectedWeapon() {
-        return this.selectedWeapon;
+    public int getSelectedWeaponNumber(int player) {
+        return this.selectedWeapon[player];
     }
 
-    public GraphicObject getWeapon() {
-        return this.ammoList.get(this.selectedWeapon);
+    public GraphicObject getPlayerWeapon(int player) {
+        return this.ammoLists.get(player).get(this.selectedWeapon[player]);
     }
 
     public void setGravity(double[] newGravity) {
@@ -85,10 +102,6 @@ public class GameStatus {
     }
 
     public List<GraphicObject> getAmmolist() {
-        return this.ammoList;
-    }
-
-    public GraphicObject getAmmo(int ammo) {
-        return this.ammoList.get(ammo);
+        return this.ammoListPlayer1;
     }
 }
