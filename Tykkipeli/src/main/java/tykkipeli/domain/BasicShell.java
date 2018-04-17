099da3373245fca46fbadb966ab2616908ace010
Oskari <oskari.tommiska@helsinki.fi>
2018-04-17 18:10:23 +0300

    Update BasicShell.java

--- Tykkipeli/src/main/java/tykkipeli/domain/BasicShell.java
+++ Tykkipeli/src/main/java/tykkipeli/domain/BasicShell.java
@@ -1,17 +1,17 @@
 package tykkipeli.domain;
 
 public class BasicShell extends GraphicObject {
-    
+
     public BasicShell() {
-        
+
     }
-    
+
     public BasicShell(double x, double y) {
         super(x, y);
     }
-    
+
     public BasicShell(double x, double y, double mass, double velocityX, double velocityY, double accelerationX, double accelerationY) {
         super(x, y, mass, velocityX, velocityY, accelerationX, accelerationY);
     }
-    
+
 }
