389e79a0656f5226f6d8b989bbe28de2db40186e
Oskari <oskari.tommiska@helsinki.fi>
2018-04-17 10:55:28 +0300

    Add Player.java

---
+++ Tykkipeli/src/main/java/tykkipeli/domain/Player.java
@@ -0,0 +1,31 @@
+package tykkipeli.domain;
+
+public class Player {
+
+    private final int num;
+    private final Cannon cannon;
+    private boolean human;
+
+    public Player(int playerNumber, Cannon playerCannon, boolean isPlayerHuman) {
+        this.num = playerNumber;
+        this.cannon = playerCannon;
+        this.human = isPlayerHuman;
+    }
+
+    public int getPlayerNum() {
+        return this.num;
+    }
+
+    public Cannon getPlayerCannon() {
+        return this.cannon;
+    }
+
+    public void setPlayerHumanStatus(boolean isPlayerHuman) {
+        this.human = isPlayerHuman;
+    }
+
+    public boolean getPlayerHumanStatus() {
+        return this.human;
+    }
+
+}
