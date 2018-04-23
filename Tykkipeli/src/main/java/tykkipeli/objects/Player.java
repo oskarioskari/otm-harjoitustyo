f448ce4abffc47d3f10659eb6f34b357fe5fdcb2
Oskari <oskari.tommiska@helsinki.fi>
2018-04-23 14:32:46 +0300

    Players now have health

--- Tykkipeli/src/main/java/tykkipeli/objects/Player.java
+++ Tykkipeli/src/main/java/tykkipeli/objects/Player.java
@@ -5,11 +5,13 @@ public class Player {
     private final int num;
     private final Cannon cannon;
     private boolean human;
+    private int health;
 
     public Player(int playerNumber, Cannon playerCannon) {
         this.num = playerNumber;
         this.cannon = playerCannon;
         this.human = true;
+        this.health = 100;
     }
 
     public int getPlayerNum() {
@@ -26,6 +28,18 @@ public class Player {
 
     public boolean getPlayerHumanStatus() {
         return this.human;
+    }
+    
+    public int getHealth() {
+        return this.health;
+    }
+    
+    public void setHealth(int newHealth) {
+        this.health = newHealth;
+    }
+    
+    public void addHealth(int amount) {
+        this.health += amount;
     }
 
 }
