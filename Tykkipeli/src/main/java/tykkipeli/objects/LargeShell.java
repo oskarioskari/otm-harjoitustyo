c0361df0f2110bbbbcec07a638c8406e7e2de769
Oskari <oskari.tommiska@helsinki.fi>
2018-04-24 12:12:43 +0300

    Add new kind of ammo

---
+++ Tykkipeli/src/main/java/tykkipeli/objects/LargeShell.java
@@ -0,0 +1,21 @@
+package tykkipeli.objects;
+
+public class LargeShell extends Ammo {
+    
+    private final double damage;
+    
+    public LargeShell() {
+        this.damage = 25;
+    }
+    
+    public LargeShell(Vector startLocation) {
+        super(startLocation);
+        this.damage = 25;
+    }
+
+    public LargeShell(Vector startLocation, Vector startVelocity, Vector startAcceleration, double mass) {
+        super(startLocation, startVelocity, startAcceleration, mass);
+        this.damage = 25;
+    }
+    
+}
