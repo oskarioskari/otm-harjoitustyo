c07b1c1d2fc5b98968da25cb6e6c9b06b249ee5a
Oskari <oskari.tommiska@helsinki.fi>
2018-04-24 12:12:20 +0300

    Add class 'Ammo'

---
+++ Tykkipeli/src/main/java/tykkipeli/objects/Ammo.java
@@ -0,0 +1,25 @@
+package tykkipeli.objects;
+
+public class Ammo extends GraphicObject {
+    
+    private final double damage;
+    
+    public Ammo() {
+        this.damage = 10;
+    }
+    
+    public Ammo(Vector startLocation) {
+        super(startLocation);
+        this.damage = 10;
+    }
+    
+    public Ammo(Vector startLocation, Vector startVelocity, Vector startAcceleration, double mass) {
+        super(startLocation, startVelocity, startAcceleration, mass);
+        this.damage = 10;
+    }
+    
+    public double getDamage() {
+        return this.damage;
+    }
+    
+}
