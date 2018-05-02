6b702740443b72c3a5bf0e1e0b3e5a4715a970ac
Oskari <oskari.tommiska@helsinki.fi>
2018-05-02 15:13:19 +0300

    Add GameAiTest.java

---
+++ Tykkipeli/src/test/java/tykkipeli/logic/GameAiTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tykkipeli.logic;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author oskari
+ */
+public class GameAiTest {
+    
+    public GameAiTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
