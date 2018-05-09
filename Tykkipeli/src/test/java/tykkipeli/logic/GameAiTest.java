afae751623ffe1ebdc309a8f2e05df91e4fa7fea
Oskari <oskari.tommiska@helsinki.fi>
2018-05-09 21:35:13 +0300

    Write tests

--- Tykkipeli/src/test/java/tykkipeli/logic/GameAiTest.java
+++ Tykkipeli/src/test/java/tykkipeli/logic/GameAiTest.java
@@ -1,42 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tykkipeli.logic;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
  * @author oskari
  */
 public class GameAiTest {
-    
+
+    private GameAi ai;
+    private GameStatus status;
+
     public GameAiTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
     @Before
     public void setUp() {
+        this.status = new GameStatus();
+        this.ai = new GameAi(status);
     }
-    
+
     @After
     public void tearDown() {
     }
-    
-    // Nothing here yet (TODO)
-    
+
+    @Test
+    public void getDefaultDifficulty() {
+        int cmp = 1;
+        int ret = ai.getDifficulty();
+        assertEquals(cmp, ret);
+    }
+
+    @Test
+    public void setDifficultyToTwo() {
+        ai.setDifficulty(2);
+        int cmp = 2;
+        int ret = ai.getDifficulty();
+        assertEquals(cmp, ret);
+    }
+
+    @Test
+    public void playingWithEasyChangesAim() {
+        ai.setDifficulty(1);
+        ai.play(1);
+        double cmp = 20;
+        double ret = status.getPlayer(1).getPlayerCannon().getCannonPower();
+        assertThat(cmp, is(not(ret)));
+    }
+
+    @Test
+    public void playingWithNormalChangesAim() {
+        ai.setDifficulty(2);
+        status.getPlayer(0).getPlayerCannon().setCannonPower(1);
+        ai.play(1);
+        double cmp = 20;
+        double ret = status.getPlayer(1).getPlayerCannon().getCannonPower();
+        assertThat(cmp, is(not(ret)));
+    }
+
+    @Test
+    public void playingWithHardChangesAim() {
+        ai.setDifficulty(3);
+        ai.play(1);
+        double cmp = 20;
+        double ret = status.getPlayer(1).getPlayerCannon().getCannonPower();
+        assertThat(cmp, is(not(ret)));
+    }
+
+    @Test
+    public void playingWithNonexistingDifficulty() {
+        ai.setDifficulty(999);
+        ai.play(1);
+        double cmp = 20;
+        double ret = status.getPlayer(1).getPlayerCannon().getCannonPower();
+        assertThat(cmp, is(not(ret)));
+    }
+
 }
