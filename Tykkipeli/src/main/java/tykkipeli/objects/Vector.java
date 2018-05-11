e39326442ee2f3c9f738e1325fe27f64076c5ed4
Oskari <oskari.tommiska@helsinki.fi>
2018-05-11 08:49:51 +0300

    Add documentation

--- Tykkipeli/src/main/java/tykkipeli/objects/Vector.java
+++ Tykkipeli/src/main/java/tykkipeli/objects/Vector.java
@@ -1,15 +1,30 @@
 package tykkipeli.objects;
 
+/**
+ * Class for Vector object. Vector can be used for location, velocity and
+ * acceleration vectors.
+ *
+ * @author oskari
+ */
 public class Vector {
 
     private double x;
     private double y;
 
+    /**
+     * Constructor for Vector class. Vector components are set to x=0 and y=0.
+     */
     public Vector() {
         this.x = 0;
         this.y = 0;
     }
 
+    /**
+     * Constructor for Vector class.
+     *
+     * @param x Set x component value
+     * @param y Set y component value
+     */
     public Vector(double x, double y) {
         this.x = x;
         this.y = y;
